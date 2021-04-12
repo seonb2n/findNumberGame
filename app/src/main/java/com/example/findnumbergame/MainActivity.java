56ca0bca4b9f0d66bf24cb96e480c91995d2042e
seonb2n <626ksb@gmail.com>
2021-04-12 21:02:01 +0900

    기본적인 화면 구성 완료, 각 view java 코드에 연결 완료

--- app/src/main/java/com/example/findnumbergame/MainActivity.java
+++ app/src/main/java/com/example/findnumbergame/MainActivity.java
@@ -3,12 +3,67 @@ package com.example.findnumbergame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textViewScore;
+    TextView textViewTimer;
+
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+    Button button5;
+    Button button6;
+    Button button7;
+    Button button8;
+    Button button9;
+    Button button10;
+    Button button11;
+    Button button12;
+    Button button13;
+    Button button14;
+    Button button15;
+    Button button16;
+    Button button17;
+    Button button18;
+    Button button19;
+    Button button20;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewScore = findViewById(R.id.textViewScore);
+        textViewTimer = findViewById(R.id.textViewTimer);
+
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+        button5 = findViewById(R.id.button5);
+        button6 = findViewById(R.id.button6);
+        button7 = findViewById(R.id.button7);
+        button8 = findViewById(R.id.button8);
+        button9 = findViewById(R.id.button9);
+        button10 = findViewById(R.id.button10);
+        button11 = findViewById(R.id.button11);
+        button12 = findViewById(R.id.button12);
+        button13 = findViewById(R.id.button13);
+        button14 = findViewById(R.id.button14);
+        button15 = findViewById(R.id.button15);
+        button16 = findViewById(R.id.button16);
+        button17 = findViewById(R.id.button17);
+        button18 = findViewById(R.id.button18);
+        button19 = findViewById(R.id.button19);
+        button20 = findViewById(R.id.button20);
+
+
+
+
     }
 }
