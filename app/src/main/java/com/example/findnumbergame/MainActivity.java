e6fc2be7a4b949eb93db110d3ccec655726068a3
seonb2n <626ksb@gmail.com>
2021-04-18 14:43:09 +0900

    JSP 와 통신을 통해서 oracle db 에 있는 랭킹을 긁어오려고 시도중. 현재 통신 결과 값이 null 값이라 해결책 찾는 중..

--- app/src/main/java/com/example/findnumbergame/MainActivity.java
+++ app/src/main/java/com/example/findnumbergame/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.findnumbergame;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
@@ -32,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
     static int score;
     static int targetNumber = 0;
+    static final int INTERNET_PERMISSON=1;
 
     Button.OnClickListener clickListener;
 
@@ -83,6 +88,26 @@ public class MainActivity extends AppCompatActivity {
                 gameStart();
             }
         });
+
+        int permissonCheck= ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET);
+
+        if(permissonCheck == PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(getApplicationContext(), "인터넷 접속 권한 있음", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(getApplicationContext(), "인터넷 접속 권한 있음", Toast.LENGTH_SHORT).show();
+
+            //권한설정 dialog에서 거부를 누르면
+            //ActivityCompat.shouldShowRequestPermissionRationale 메소드의 반환값이 true가 된다.
+            //단, 사용자가 "Don't ask again"을 체크한 경우
+            //거부하더라도 false를 반환하여, 직접 사용자가 권한을 부여하지 않는 이상, 권한을 요청할 수 없게 된다.
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.INTERNET)){
+                //이곳에 권한이 왜 필요한지 설명하는 Toast나 dialog를 띄워준 후, 다시 권한을 요청한다.
+                Toast.makeText(getApplicationContext(), "인터넷 접속 권한이 필요합니다", Toast.LENGTH_SHORT).show();
+                ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.INTERNET}, INTERNET_PERMISSON);
+            }else{
+                ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.RECEIVE_SMS}, INTERNET_PERMISSON);
+            }
+        }
 
         clickListener = new Button.OnClickListener() {
             @Override
--- app/src/main/java/com/example/findnumbergame/RankActivity.java
+++ app/src/main/java/com/example/findnumbergame/RankActivity.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -20,20 +21,46 @@ import androidx.appcompat.app.AppCompatActivity;
 public class RankActivity extends AppCompatActivity {
 
     static String name;
+    static String rankData = null;
+    TextView[] textViewNames = new TextView[5];
+    TextView[] textViewScores = new TextView[5];
+
+
 
     @Override
     public void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rank);
 
-        //Oracle DB 로부터 데이터를 받아와서 랭킹 화면 구성
+        textViewNames[0] = findViewById(R.id.textViewNameOne);
+        textViewNames[1] = findViewById(R.id.textViewNameTwo);
+        textViewNames[2] = findViewById(R.id.textViewNameThree);
+        textViewNames[3] = findViewById(R.id.textViewNameFour);
+        textViewNames[4] = findViewById(R.id.textViewNameFive);
+
+        textViewScores[0] = findViewById(R.id.textViewScoreOne);
+        textViewScores[1] = findViewById(R.id.textViewScoreTwo);
+        textViewScores[2] = findViewById(R.id.textViewScoreThree);
+        textViewScores[3] = findViewById(R.id.textViewScoreFour);
+        textViewScores[4] = findViewById(R.id.textViewScoreFive);
+
+
+        String sendMsg = "update_rank";
+        String result  = null;
+
+        try {
+            result = new Task("update_rank").execute().get();
+            rankData = result;
+            Toast.makeText(this, result, Toast.LENGTH_LONG).show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         name = showNameDialog();
 
         Intent intent = getIntent();
         int score = intent.getIntExtra("Score", -9999);
-        Toast.makeText(this,"Score is " + score, Toast.LENGTH_LONG).show();
-
         //score 가 랭킹에 들면 화면에 반영
         //안들면 Toast Message 출력
 
@@ -53,5 +80,23 @@ public class RankActivity extends AppCompatActivity {
                 }).show();
 
         return name;
+    }
+
+    private void rankUpdater(String rankData) {
+        String str= rankData;
+        String[] array = str.split("$");
+        String[] rankerName = new String[6];
+        String[] rankerScore = new String [6];
+        for(int i = 0; i<array.length; i++){
+            rankerName[i] = array[i].substring(0, array[i].indexOf("#"));
+            rankerScore[i] = array[i].substring(array[i].lastIndexOf("#")+1);
+        }
+
+        for(int i = 0; i < 5; i++) {
+            textViewNames[i].setText(rankerName[i]);
+            textViewScores[i].setText(rankerScore[i]);
+
+        }
+
     }
 }
